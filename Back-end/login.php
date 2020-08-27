f05743c95fb843f491be7c5fcfa69c7db7d9a1fd
Cfaust <cfaust@student.neumont.edu>
2020-08-26 18:43:21 -0600

    cookie uses user id, get user reviews

--- Back-end/login.php
+++ Back-end/login.php
@@ -5,13 +5,17 @@
   include_once 'database.php';
   $database = new Database();
   $connectionDB = $database->ConnectToDatabase();
+  $resultArray = array();
 
   $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 
   $result = $connectionDB->query($sql);
   if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      $resultArray[] = $row;
+    }
     $cookie_name = "login";
-    setcookie($cookie_name, $username, time() + (86400 * 30), "/"); // 86400 = 1 day
+    setcookie($cookie_name, $resultArray["0"]["id"], time() + (86400 * 30), "/"); // 86400 = 1 day
     header("Location: http://localhost:9090/SolutionStack_RE/Front-end/home.php");
   } else {
     header("Location: http://localhost:9090/SolutionStack_RE/Front-end/login.php");
