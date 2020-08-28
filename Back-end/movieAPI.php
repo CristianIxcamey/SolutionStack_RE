c1db6e9ae935b7c161041fc28fb15de912ea9cc7
Cfaust <cfaust@student.neumont.edu>
2020-08-28 15:28:30 -0600

    movie page

--- Back-end/movieAPI.php
+++ Back-end/movieAPI.php
@@ -1,10 +1,10 @@
 <?php
 // These are all the headers to make sure that there are no issues when using the api
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Allow-Credentials: true");
+// header("Access-Control-Allow-Origin: *");
+// header("Content-Type: application/json; charset=UTF-8");
+// header("Access-Control-Allow-Headers: access");
+// header("Access-Control-Allow-Methods: GET");
+// header("Access-Control-Allow-Credentials: true");
 
 $start = 'https://api.themoviedb.org/3/';
 $key = '?api_key=917bf8547464514e193b8bc4841df69c';
@@ -17,6 +17,12 @@ class MovieApi{
         return $movies;
     }
 
+    public function getMovieData($id)
+    {
+        $movies = file_get_contents("https://api.themoviedb.org/3/movie/530915?api_key=917bf8547464514e193b8bc4841df69c");
+        $movies = json_decode($movies);
+        return $movies;
+    }
 
 
 }
--- Front-end/movie.php
+++ Front-end/movie.php
@@ -7,13 +7,32 @@
     <title>Review A lot</title>
 </head>
 
+<?php
+$movieId = $_GET['id'];
+include '../Back-end/movieAPI.php';
+$movie = new MovieApi();
+$movieData = $movie->getMovieData($movieId);
+// echo json_encode($movieData);
+
+$userId = $_COOKIE['login'];
+$username = $_COOKIE['username'];
+
+$movieName = $movieData->original_title;
+$moviePoster = "https://image.tmdb.org/t/p/original".$movieData->poster_path;
+$movieDescription = $movieData->overview;
+?>
+
 <body>
-    <?php
-    $userId = $_COOKIE['login'];
-    $movieid = "newMovie123";
-    $movieName = "Kong";
-    $username = $_COOKIE['username'];
-    ?>
+
+    <div class="movieHolder">
+        <div class="moviePoster">
+            <img src="<?php echo "$moviePoster"; ?>" alt="moviePoster">
+        </div>
+        <div class="movieData">
+            <h1 class="movieName"><?php echo "$movieName"; ?></h1>
+            <p class="movieDes"><?php echo "$movieDescription"; ?></p>
+        </div>
+    </div>
 
     <form name="form" action="../Back-end/create_review_post.php" method="post">
         <label for="rating">Review Rating:</label>
@@ -26,7 +45,7 @@
         </select>
         <input type="text" name="reviewMessage" placeholder="Review Message">
         <input type='hidden' name='userId' value='<?php echo "$userId"; ?>' />
-        <input type='hidden' name='movieid' value='<?php echo "$movieid"; ?>' />
+        <input type='hidden' name='movieid' value='<?php echo "$movieId"; ?>' />
         <input type='hidden' name='movieName' value='<?php echo "$movieName"; ?>' />
         <input type='hidden' name='username' value='<?php echo "$username"; ?>' />
         <input type="submit" name="submit" value="Create Review" />
@@ -35,14 +54,14 @@
     include '../Back-end/read_movie.php';
     $data = new reader();
     // Get movie id out of url
-    $res = $data->read("newMovie123");
+    $res = $data->read($movieId);
 
-    // foreach ($res["data"] as $id => $content) {
-    //     echo "<div>";
-    //     echo "<h3 class='reviewTitle'>$content[username] - $content[rating] out of 5 stars</h3>";
-    //     echo "<p class='reviewDescription'>$content[description]";
-    //     echo "</div>";
-    // }
+    foreach ($res["data"] as $id => $content) {
+        echo "<div>";
+        echo "<h3 class='reviewTitle'>$content[username] - $content[rating] out of 5 stars</h3>";
+        echo "<p class='reviewDescription'>$content[description]";
+        echo "</div>";
+    }
     ?>
 
 </body>
