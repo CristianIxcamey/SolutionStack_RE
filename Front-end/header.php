5a11f8113ba45bedc8febdc0798427f1eec20277
Cixcamey <cixcamey@gmail.com>
2020-08-28 16:55:48 -0600

    movie display styling

--- Front-end/header.php
+++ Front-end/header.php
@@ -1,19 +1,21 @@
 <!-- Variables -->
 <?php
- $title = "Review Everything";
+$title = "Review Everything";
 ?>
 
 <!-- Header stuff -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <link rel="stylesheet" href="./style.css">
-    <title><?php echo "{$title}"?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css" type="text/css">
+    <title><?php echo "{$title}" ?></title>
 </head>
+
 <body>
 
 
     <!-- Requiring Menu in header so i dont have to repeat on each page  -->
-    <?php require "nav.php";?>
+    <?php require "nav.php"; ?>
--- Front-end/movie.php
+++ Front-end/movie.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Review A lot</title>
+    <link type="text/css" rel="stylesheet" href="./style.css">
+    <link href="style.css" media="all" rel="stylesheet" type="text/css" />
 </head>
 
 <?php
@@ -18,52 +20,61 @@ $userId = $_COOKIE['login'];
 $username = $_COOKIE['username'];
 
 $movieName = $movieData->original_title;
-$moviePoster = "https://image.tmdb.org/t/p/original".$movieData->poster_path;
+$moviePoster = "https://image.tmdb.org/t/p/original" . $movieData->poster_path;
+$backgroundPoster = "https://image.tmdb.org/t/p/original" . $movieData->backdrop_path;
 $movieDescription = $movieData->overview;
 ?>
 
 <body>
-
+    <img class="backImg" src="<?php echo "$backgroundPoster"; ?>">
     <div class="movieHolder">
-        <div class="moviePoster">
-            <img src="<?php echo "$moviePoster"; ?>" alt="moviePoster">
-        </div>
+        <img class="moviePoster" src="<?php echo "$moviePoster"; ?>" alt="moviePoster">
         <div class="movieData">
             <h1 class="movieName"><?php echo "$movieName"; ?></h1>
             <p class="movieDes"><?php echo "$movieDescription"; ?></p>
         </div>
     </div>
 
-    <form name="form" action="../Back-end/create_review_post.php" method="post">
-        <label for="rating">Review Rating:</label>
-        <select name="rating" id="pages">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-        </select>
-        <input type="text" name="reviewMessage" placeholder="Review Message">
-        <input type='hidden' name='userId' value='<?php echo "$userId"; ?>' />
-        <input type='hidden' name='movieid' value='<?php echo "$movieId"; ?>' />
-        <input type='hidden' name='movieName' value='<?php echo "$movieName"; ?>' />
-        <input type='hidden' name='username' value='<?php echo "$username"; ?>' />
-        <input type="submit" name="submit" value="Create Review" />
-    </form>
-    <?php
-    include '../Back-end/read_movie.php';
-    $data = new reader();
-    // Get movie id out of url
-    $res = $data->read($movieId);
+    <div class="writeReview">
+        <h2>Write A Review</h2>
+        <form class="reviewForm" name="form" action="../Back-end/create_review_post.php" method="post">
+            <div class="ratingSection">
+                <label for="rating">Review Rating:</label>
+                <select name="rating" id="pages">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
+            <input class="textInput" type="textarea" name="reviewMessage" placeholder="Review Message">
+            <input type='hidden' name='userId' value='<?php echo "$userId"; ?>' />
+            <input type='hidden' name='movieid' value='<?php echo "$movieId"; ?>' />
+            <input type='hidden' name='movieName' value='<?php echo "$movieName"; ?>' />
+            <input type='hidden' name='username' value='<?php echo "$username"; ?>' />
+            <input class="submitReview" type="submit" name="submit" value="Create Review" />
+        </form>
+    </div>
 
-    foreach ($res["data"] as $id => $content) {
-        echo "<div>";
-        echo "<h3 class='reviewTitle'>$content[username] - $content[rating] out of 5 stars</h3>";
-        echo "<p class='reviewDescription'>$content[description]";
-        echo "</div>";
-    }
-    ?>
+    <div class="allReviews">
 
+        <?php
+        include '../Back-end/read_movie.php';
+        $data = new reader();
+        // Get movie id out of url
+        $res = $data->read($movieId);
+
+        foreach ($res["data"] as $id => $content) {
+            echo "<div class= 'review'>";
+            echo "<h2 class='username'>$content[username]</h2>";
+            echo "<h3 class='reviewTitle'>$content[rating] out of 5 stars</h3>";
+            echo "<p class='reviewDescription'>$content[description]";
+            echo "</div>";
+        }
+        ?>
+
+    </div>
 </body>
 
 </html>
